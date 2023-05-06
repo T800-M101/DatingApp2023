9d0935c72a76a6d49fa1b23b4d1e9ed2a5c8528f
memo moran <tuahil@gmail.com>
2023-05-06 08:42:56 -0600

    Adding boostrap

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
+// It is importante the order of the middleware
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); // x or builder is the CORS Policy Builder
 app.MapControllers();
 
 app.Run();
